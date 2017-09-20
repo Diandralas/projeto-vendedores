22c8870e451b3adf5cf7b8fecc9aaea0c27802e3
Bruno <brunoapimentel@gmail.com>
2017-09-19 21:34:28 -0300

    Refatoração do projeto

--- index.js
+++ index.js
@@ -1,81 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const vendedoresController = require('./controllers/vendedores');
+const loginController = require('./controllers/login');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost/vendedores');
+
 app.use(bodyParser.json());
-
-let dados = {
-    teste: "xpto",
-    teste2: "info-legal"
-}
-
-const VendedorSchema = mongoose.model('Vendedor', {
-    nome: {type: String, required: true},
-    email: {type: String, required: true},
-    telefone: String,
-    senha: {type: String, required: true}
-});
-
-app.get('/', (request, response) => {
-    response.send(dados);
-});
-
-app.get('/vendedores', (request, response) => {
-    VendedorSchema.find((error, vendedores) => {
-      response.send(vendedores);
-    })
-});
-
-app.get('/vendedor/:id', (request, response) => {
-    VendedorSchema.findById(request.params.id, (error, vendedor) => {
-        if(vendedor){
-            response.send(vendedor);
-            return;
-        }
-
-        response.sendStatus(404);
-    })
-});
-
-app.get('/vendedores/:nome', (request, response) => {
-    const regex = new RegExp(request.params.nome, 'i');
-
-    VendedorSchema.find({nome: regex}, (error, vendedor) => {
-        if(vendedor){
-            response.send(vendedor);
-            return;
-        }
-
-        response.sendStatus(404);
-    })
-});
-
-app.post('/vendedores', (request, response) => {
-    let vendedor = new VendedorSchema(request.body);
-
-    vendedor.save((error, resultado) => {
-        if(error){
-            response.status(400).send(error);
-            return;
-        }
-
-        response.status(201).send(resultado);
-    });
-});
-
-app.post('/login', (request, response) => {
-    VendedorSchema.findOne(request.body, (error, vendedor) => {
-        if(vendedor){
-            response.send(vendedor);
-            return;
-        }
-
-        response.sendStatus(400);
-    });
-});
+app.use('/vendedores', vendedoresController);
+app.use('/login', loginController);
 
 app.listen(3000, () => {
     console.log('Servidor inicializado');
